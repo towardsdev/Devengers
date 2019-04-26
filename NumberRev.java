715fa4c970cf62ad05e31f510905947ec5b02a0c
kaneganti <49714746+kaneganti@users.noreply.github.com>
2019-04-26 18:58:04 +0530

    updated
    test

--- NumberRev.java
+++ NumberRev.java
@@ -3,15 +3,16 @@ public class NumberRev {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int no=533203;
+		int no1=no;
 		int len=String.valueOf(no).length();
 		int modVal=0;
 		System.out.println("Number reverse");
 		for(int i=1;i<=len;i++){
 			modVal=modVal*10;
 			modVal=modVal+no%10;
-			System.out.println("Mod value of i="+i+" = "+modVal);
 			no=no/10;
 		}
+		System.out.println("original number :"+no1+" Reversed number :"+modVal);
 	}
 
 }
